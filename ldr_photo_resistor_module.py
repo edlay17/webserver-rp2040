c315e3fb033234924c8582ed2aa3469d461375aa
Eduard Chyzhyk <e.chyzhyk@mageworx.com>
2023-06-10 11:35:34 +0200

    create web server using microdot

---
+++ ldr_photo_resistor_module.py
@@ -0,0 +1,12 @@
+import machine
+ 
+class LDR:
+    def __init__(self, pin):
+        self.ldr_pin = test
+        
+    def get_raw_value(self):
+        return 'test'
+    
+    def get_light_percentage(self):
+        return round(100)
+ 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from microdot import Microdot, Response
 from microdot_utemplate import render_template
+import time
 
 app = Microdot()
 Response.default_content_type = 'text/html'
@@ -9,19 +10,20 @@ def index(request):
     return 'Hello, world!'
 
 @app.route('/orders', methods=['GET'])
-def index(req):
+def index2(req):
     gamename = "cool game"
     score = ["1000", "2000", "3000"]
 
+    #return 'orders'
     return render_template('game.html', gamename=gamename, score=score)
 
-@app.route('/ws')
-@with_websocket
-async def read_sensor(request, ws):
-    while True:
-        # data = await ws.receive()
-        time.sleep(.1)
-        await ws.send(str('hello ws'))
+#@app.route('/ws')
+#@with_websocket
+#async def read_sensor(request, ws):
+#    while True:
+        # data = await ws.receive()W
+#        time.sleep(.1)
+#        await ws.send(str('hello ws'))
 
 if __name__ == '__main__':
     app.run(debug=True)
